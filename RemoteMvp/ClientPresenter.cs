571ff401aa47ff46cab238aef2bacb0740ec5bfb
s2210438014 <s2210438014@fhooe.at>
2023-06-20 14:17:37 +0200

    Copy Presenter to Admin Add UserType to Ctor

--- RemoteMvp/ClientPresenter.cs
+++ RemoteMvp/ClientPresenter.cs
@@ -37,19 +37,19 @@ namespace RemoteMvpClient
 
         private async void OnLoginRequested(object? sender, Tuple<string, string> e)
         {
-            RemoteActionRequest loginRequest = new RemoteActionRequest(ActionType.Login, e.Item1, e.Item2);
+            RemoteActionRequest loginRequest = new RemoteActionRequest(ActionType.Login, e.Item1, e.Item2,UserType.User);
             await ProcessRequest(loginRequest);
         }
 
         private async void OnRegisterRequested(object? sender, Tuple<string, string> e)
         {
-            RemoteActionRequest loginRequest = new RemoteActionRequest(ActionType.Register, e.Item1, e.Item2);
+            RemoteActionRequest loginRequest = new RemoteActionRequest(ActionType.Register, e.Item1, e.Item2,UserType.User);
             await ProcessRequest(loginRequest);
         }
 
         private async void OnDeleteRequested(object? sender, Tuple<string, string> e)
         {
-            RemoteActionRequest deleteRequest = new RemoteActionRequest(ActionType.Delete, e.Item1, e.Item2);
+            RemoteActionRequest deleteRequest = new RemoteActionRequest(ActionType.Delete, e.Item1, e.Item2,U);
             await ProcessRequest(deleteRequest);
         }
 
