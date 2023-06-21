f0d19db8365c8625ce2ee9f9efff40b8eaef447b
s2210438014 <s2210438014@fhooe.at>
2023-06-21 11:14:20 +0200

    Implement Model Add lsitView and Btn to View

--- RemoteMVPAdmin/AdminModel.cs
+++ RemoteMVPAdmin/AdminModel.cs
@@ -8,5 +8,25 @@ namespace RemoteMVPAdmin
 {
     internal class AdminModel
     {
+        public List<Tuple<string, string>> _users;
+
+        public AdminModel()
+        {
+            _users = new List<Tuple<string, string>>();
+        }
+
+        public void AddToList(string user)
+        {
+            var parts = user.Split(';');
+            _users.Add(Tuple.Create(parts[0], parts[1]));
+        }
+
+        public void ClearList()
+        {
+            _users.Clear();
+        }
+
+
+
     }
 }
